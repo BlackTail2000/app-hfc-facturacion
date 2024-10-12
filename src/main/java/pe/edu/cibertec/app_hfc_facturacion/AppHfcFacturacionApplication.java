ecee9bac637ac24c207bb9d2faf76b463e189558
BlackTail2000 <carlosgabrielbacamanrique@hotmail.com>
2024-10-12 00:32:32 -0500

    Proyecto Creado

---
+++ src/main/java/pe/edu/cibertec/app_hfc_facturacion/AppHfcFacturacionApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_hfc_facturacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppHfcFacturacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppHfcFacturacionApplication.class, args);
+	}
+
+}
